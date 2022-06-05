7860d14944e297db786f446ff64b6a9ff0d1ad16
Tomás Bruno <tomas.bruno@ing.austral.edu.ar>
2022-06-05 15:14:11 -0300

    Added Keycloak and Nginx

--- src/data/api/apiPostData.ts
+++ src/data/api/apiPostData.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import {FullPost, NewPost, Post, PostData} from "../posts";
+import UserService from "../../utils/UserService";
 
 export class ApiPostData implements PostData{
 
@@ -12,7 +13,7 @@ export class ApiPostData implements PostData{
     }
 
     getFeedPosts(): Promise<Post[]> {
-        return postAxios.get("/");
+        return postAxios.get("/").then(res => res.data);
     }
 
     answerPost(postId: string, answer: NewPost): Promise<FullPost> {
@@ -23,15 +24,26 @@ export class ApiPostData implements PostData{
 
 const postAxios = axios.create(
     {
-        baseURL: "http://localhost:8082/post",
+        baseURL: "http://localhost:8084/post",
         headers: {
-            "Authorization": "Bearer " + localStorage.getItem("token"),
+            // "Authorization": "Bearer " + UserService.getToken(),
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
             "crossDomain": true
         }
     }
 )
+
+postAxios.interceptors.request.use((config) => {
+    if (UserService.isLoggedIn()) {
+        const cb = () => {
+            // @ts-ignore
+            config.headers.Authorization = `Bearer ${UserService.getToken()}`;
+            return Promise.resolve(config);
+        };
+        return UserService.updateToken(cb);
+    }
+});
 
 export const getPostsFor = async (userId: string) => {
     return await postAxios.get(`/user/${userId}`)
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { App }  from './main/app';
 import theme from './theme';
+import UserService from './utils/UserService';
+import HttpService from "./utils/HttpService";
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement!);
+// const root = createRoot(rootElement!);
 
-root.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
-);
+const renderApp = () => createRoot(rootElement!);
+
+const keycloakPromise = UserService.initKeycloak();
+keycloakPromise.then((authenticated: boolean) => {
+    if (!authenticated) {
+        console.log("user is not authenticated..!");
+    }
+    console.log("user is logged in!")
+    renderApp().render(
+        <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, conse baseline to build upon. */}
+            <CssBaseline />
+            <App />
+        </ThemeProvider>,
+    );
+}).catch(console.error);
+HttpService.configure();
